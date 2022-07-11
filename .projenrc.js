89d5d7821e3fded5a83c7c593402e5a396b76ebf
Momo Kornher <kornherm@amazon.co.uk>
2022-07-11 13:52:34 +0100

    chore: manual release

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,5 @@
 const { awscdk } = require('projen');
+const { ReleaseTrigger } = require('projen/lib/release');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Momo Kornher',
   authorAddress: 'mail@moritzkornher.de',
@@ -6,10 +7,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
   defaultReleaseBranch: 'main',
   name: '@mrgrain/new-project-test',
   repositoryUrl: 'https://github.com/mrgrain/new-project-test.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  releaseTrigger: ReleaseTrigger.manual(),
 });
 project.synth();
